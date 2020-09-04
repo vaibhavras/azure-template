ef997f3abae51f66626c24865be2edf25f5c0cc1
Rastogi, Vaibhav (VW IT Services India) <vaibhav.rastogi@volkswagen.co.in>
2020-09-04 20:18:07 +0530

    changes

--- src/main/java/com/azure/azuretemplate/restcontroller/Sample.java
+++ src/main/java/com/azure/azuretemplate/restcontroller/Sample.java
@@ -3,11 +3,16 @@ package com.azure.azuretemplate.restcontroller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Sample {
+
+    @Autowired
+    private Environment env;
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -15,6 +20,7 @@ public class Sample {
     @ResponseStatus(HttpStatus.OK)
     public String hello() {
         logger.info("From hello().");
+        logger.info("print java home {}", env.getProperty("JAVA_HOME"));
         return "Hello from Azure template poc";
     }
 
