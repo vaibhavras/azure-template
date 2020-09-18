cc2de6520a6ad579012ea7228f3c4feffea40968
vaibhavrastogi <vaibhav.rastogi5g@gmail.com>
2020-09-18 12:15:41 +0530

    unit test case

---
+++ src/test/java/com/azure/azuretemplate/restcontroller/SampleTestUnitTestController.java
@@ -0,0 +1,28 @@
+package com.azure.azuretemplate.restcontroller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleTestUnitTestController {
+
+
+    @InjectMocks
+    private Sample sampleController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void shouldTestController() {
+        String responseEntity = sampleController.hello();
+
+
+        assertEquals("Hello from Azure template poc", responseEntity);
+//        assertEquals(responseEntity.getBody(), detectedLanguage);
+    }
+}
